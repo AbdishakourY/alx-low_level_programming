2f969b2386cb389aee67875de5dced4a0b32b1d7
AbdishakourY <abdishakour.moyousuf@gmail.com>
2022-09-08 15:57:23 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,16 +3,18 @@
 /**
  * main - Prints the size of many var types
  *
+ * Description: using the main function
+ * Tis program prints "Programming is building a mutlilingual puzzle
  * Return: Always 0(Success)
  */
 int main(void)
 {
 
-char a;
-int b;
-long int c;
-long long int d;
-float e;
+  char a;
+  int b;
+  long int c;
+  long long int d;
+  float e;
 
 printf("Size of a char: %d byte(s)\n",sizeof(a));
 printf("Size of an int: %d byte(s)\n",sizeof(b));
