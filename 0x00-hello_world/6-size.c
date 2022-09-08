4e5a6e7c54d14661092d99c899a214837604d36e
AbdishakourY <abdishakour.moyousuf@gmail.com>
2022-09-08 16:02:17 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
-
 /**
- * main - Prints the size of many var types
- *
- * Description: using the main function
- * Tis program prints "Programming is building a mutlilingual puzzle
- * Return: Always 0(Success)
- */
+* main - Prints the sizeof many var types
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-
-  char a;
-  int b;
-  long int c;
-  long long int d;
-  float e;
-
-  printf("Size of a char: %d byte(s)\n",sizeof(a));
-  printf("Size of an int: %d byte(s)\n",sizeof(b));
-  printf("Size of a long int: %d byte(s)\n",sizeof(c));
-  printf("Size of a long long int: %d byte(s)\n",sizeof(d));
-  printf("Size of a float: %d bytes(s)\n",sizeof(e));
-  return: (0);
-
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+printf("Size of a char: %d byte(s)\n", sizeof(a));
+printf("Size of an int: %d byte(s)\n", sizeof(b));
+printf("Size of a long int: %d byte(s)\n", sizeof(c));
+printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+printf("Size of a float: %d byte(s)\n", sizeof(e));
+return (0);
 }
