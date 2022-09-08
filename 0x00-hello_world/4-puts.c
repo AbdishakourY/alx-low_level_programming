b073a5685edfdac2b7b23cd88f5920ebc3f080aa
AbdishakourY <abdishakour.moyousuf@gmail.com>
2022-09-08 14:57:49 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-print the string in the put function
+ *
+ * Using main function
+ * This program prints out what is inside the puts brackets
+ * return: 0
+ */
+int main(void)
+{
+	puts(("/"Programming is like building a multilingual puzzle);
+			return(0);
+}
