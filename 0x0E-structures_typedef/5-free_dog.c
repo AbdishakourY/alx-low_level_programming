0ecf5b915ca5e6ce2b1886afafe6d035ac4d6849
AbdishakourY <abdishakour.moyousuf@gmail.com>
2022-10-10 09:11:32 -0700

    task 5 correction: removed the space from the comment section

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- * free_dog - a function that frees dogs.
- * @d: pointer to dog struct.
+ *free_dog - a function that frees dogs.
+ *@d: pointer to dog struct.
  *
- * Return: viod.
+ *Return: viod.
  */
 void free_dog(dog_t *d)
 {
